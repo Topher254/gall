aa3b659c312a1181a9f36a1f2c3ae39c0bff438b
Topher254 <raphaelsarota@gmail.com>
2026-03-14 14:57:42 +0300

    host

--- server/routes/responses.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { handleResponse, handleExpectations } = require('../controllers/responseController');
-
-router.post('/response', handleResponse);
-router.post('/expectations', handleExpectations);
-
-module.exports = router;
